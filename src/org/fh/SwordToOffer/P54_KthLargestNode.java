a314791ca5217d69f63ff0080a14c19b9b6327a4
xinke-fh <1214316932@qq.com>
2021-03-07 22:13:23 +0800

    剑指Offer P54_二叉搜索树的第k大节点

---
+++ src/org/fh/SwordToOffer/P54_KthLargestNode.java
@@ -0,0 +1,39 @@
+package org.fh.SwordToOffer;
+
+/**
+ * 二叉搜索树的第k大节点
+ */
+class KthLargestNode {
+
+    int k, kthVal;
+
+    /**
+     * 中序遍历 的 倒序的第 k 个元素
+     * @param root
+     * @param k
+     * @return
+     */
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        reInorder(root);
+        return kthVal;
+    }
+
+    /**
+     * 中序遍历的倒序
+     */
+    public void reInorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        reInorder(root.right);
+        if (--k == 0) {
+            kthVal = root.val;
+            return;
+        }
+        reInorder(root.left);
+    }
+}
+
+public class P54_KthLargestNode {
+}
